3f6e656ac773373d3faeed8b72fa2e9d9dd1c70e
tippy33 <chris5050@snu.ac.kr>
2021-06-05 13:41:39 +0900

    change data structure from vector to map

---
+++ build/main/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/pc/Desktop/HW2/Graph-Pattern-Matching-Challenge/src/backtrack.cc" "main/CMakeFiles/program.dir/__/src/backtrack.cc.obj" "gcc" "main/CMakeFiles/program.dir/__/src/backtrack.cc.obj.d"
+  "C:/Users/pc/Desktop/HW2/Graph-Pattern-Matching-Challenge/src/candidate_set.cc" "main/CMakeFiles/program.dir/__/src/candidate_set.cc.obj" "gcc" "main/CMakeFiles/program.dir/__/src/candidate_set.cc.obj.d"
+  "C:/Users/pc/Desktop/HW2/Graph-Pattern-Matching-Challenge/src/graph.cc" "main/CMakeFiles/program.dir/__/src/graph.cc.obj" "gcc" "main/CMakeFiles/program.dir/__/src/graph.cc.obj.d"
+  "C:/Users/pc/Desktop/HW2/Graph-Pattern-Matching-Challenge/main/main.cc" "main/CMakeFiles/program.dir/main.cc.obj" "gcc" "main/CMakeFiles/program.dir/main.cc.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/main/CMakeFiles/program.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/program.dir/__/src/backtrack.cc.obj"
+  "CMakeFiles/program.dir/__/src/backtrack.cc.obj.d"
+  "CMakeFiles/program.dir/__/src/candidate_set.cc.obj"
+  "CMakeFiles/program.dir/__/src/candidate_set.cc.obj.d"
+  "CMakeFiles/program.dir/__/src/graph.cc.obj"
+  "CMakeFiles/program.dir/__/src/graph.cc.obj.d"
+  "CMakeFiles/program.dir/main.cc.obj"
+  "CMakeFiles/program.dir/main.cc.obj.d"
+  "libprogram.dll.a"
+  "program.exe"
+  "program.exe.manifest"
+  "program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
